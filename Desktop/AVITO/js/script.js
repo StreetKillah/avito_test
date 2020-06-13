dfe62718720aa49e45c3cf1898f3fa123e6f2111
Michael Prokopev <matroskin119@gmail.com>
2020-06-13 13:01:29 +0300

    3rd commit

--- Desktop/AVITO/js/script.js
+++ Desktop/AVITO/js/script.js
@@ -1,18 +1,28 @@
 
 document.getElementById('submit_user').addEventListener('click', (e) => {
     e.preventDefault();
-    var user = document.getElementById('input_user').value;
+    var name = document.getElementById('input_user').value;
 
-    searchUsers(user);
+    searchUsers(name);
 });
 
-searchUsers = (user) => {
-    fetch(`https://api.github.com/users/${user}`)
+searchUsers = (name) => {
+    document.getElementById('repositories_list').innerHTML = "";
+    fetch(`https://api.github.com/search/repositories?q=${name}&sort=stars&order=desc`)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      console.log(data);
+      for(let elem in data.items){
+           console.log(data.items[elem]);
+            document.getElementById('repositories_list').insertAdjacentHTML('beforeend',`<div id='elem'><span id="name_elem">${data.items[elem].name}</span>
+            <span id="owner"><img src="${data.items[elem].owner.avatar_url}" alt="." /><div>by ${data.items[elem].owner.login}</div></span>
+            <span id="last_commit">Last commit: ${data.items[elem].updated_at.slice(0,10)}</span>
+            <a href="https://${data.items[elem].git_url.slice(6)}" id="link_to_github">Open at Github</a>
+            <span id="stars"><img src="./img/star.svg" alt="." />${data.items[elem].stargazers_count}</span>
+        </div>`);
+      } 
+      
     });
 }
 
